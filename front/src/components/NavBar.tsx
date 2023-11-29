2250ca9cbc4e1f0146c0550ad6525dbb8e378039
sydneymeza <sydney_meza@brown.edu>
2023-11-29 10:39:21 -0500

    chore: just delted some unneeded code

--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -4,47 +4,8 @@ import { Link } from "react-scroll";
 
 import Hamburger from "hamburger-react";
 
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
-function debounce(func, wait, immediate) {
-  var timeout;
-  return function () {
-    var context = this,
-      args = arguments;
-    var later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-}
 
 function NavBar() {
-  // const [prevScrollPos, setPrevScrollPos] = useState(0);
-  // const [visible, setVisible] = useState(true);
-  // const [textColor, setTextColor] = useState("white");
-  // const [bgColor, setBgColor] = useState("transparent");
-  // const [borderSize, setBorderSize] = useState("0px");
-
-  // const listenScrollEvent = (event) => {
-
-  //     setTextColor("black");
-  //     setBgColor("white");
-  //     setBorderSize("1px");
-  //     return;
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener("scroll", listenScrollEvent);
-
-  //   return () =>
-  //     window.removeEventListener("scroll", listenScrollEvent);
-  // }, []);
 
   const [isNavExpanded, setIsNavExpanded] = useState(false);
 
