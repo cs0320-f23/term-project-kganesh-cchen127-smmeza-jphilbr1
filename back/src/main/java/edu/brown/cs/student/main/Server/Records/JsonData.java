c010b3e3871553f155ab934384f3d09ea98913d6
Jay Philbrick <jay_philbrick@brown.edu>
2023-11-30 19:11:30 -0500

    feat: finished deleting backend

--- back/src/main/java/edu/brown/cs/student/main/Server/Records/JsonData.java
+++
@@ -1,7 +0,0 @@
-package edu.brown.cs.student.main.Server.Records;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-
-public record JsonData(Map<String, Object> data) { }
