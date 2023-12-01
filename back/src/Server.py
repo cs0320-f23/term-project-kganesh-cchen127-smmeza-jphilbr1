d16f4ab1323bd32eb4bb9f72a04ae2664e9274b0
Kylash Ganesh <kylash_ganesh@brown.edugit config --global user.name kylashg1>
2023-11-30 23:25:23 -0500

    feat: added generic endpoint function and used it to clean up unemployment endpoint. Also used it to create enpoint for labor force, unemployment, and employment

--- back/src/Server.py
+++ back/src/Server.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+import json
 
+
+API_KEY = "8e005747a44f4542965de8c1051a83f6"
 
 # Given latitude and longitude, returns a map of useful location information for those coords, including fips codes, county name, and state name
 def coordToStateAndCountyConversion(lat, long):
@@ -27,9 +30,9 @@ def coordToStateAndCountyConversion(lat, long):
 
     return location_info
 
-print(coordToStateAndCountyConversion(41.8240, -71.4128))
+# print(coordToStateAndCountyConversion(41.8240, -71.4128))
 
-
+# -------------------- Beggning of Server Code --------------------------
 
 app = Flask(__name__)
 
@@ -37,21 +40,96 @@ app = Flask(__name__)
 def index():
     return "Hello World!"
 
+# --------------- Generic Endpoint Function------------------------
+# Enter a data_name (ex: unemployment rate) and the corresponding measure code 
+#    (ex: 03), and it will return a return a response_map json for that type of 
+#    data
+def generic_endpoint(data_name, measure_code):
+    state_fips = request.args.get('state_fips')
+    county_fips = request.args.get('county_fips')
+    # EXAMPLE: http://127.0.0.1:5000/unemployment_rate?state_fips=01&county_fips=001
+    # api_url = "https://api.bls.gov/publicAPI/v2/timeseries/data/LAUCN" + str(state_fips) + str(county_fips) + "00000000" + measure_code
+    api_url = f"https://api.bls.gov/publicAPI/v2/timeseries/data/LAUCN{state_fips}{county_fips}00000000{measure_code}?latest=true&registrationkey={API_KEY}"
+    print(api_url)
 
-@app.route('/test')
-def hello():
-    # Access query parameters using request.args
-    name = request.args.get('name', 'Guest')
-    return f'Hello, {name}!'
+    # Making a GET request
+    response = requests.get(api_url)    
+    API_Data = response.json()
 
+    if API_Data["status"] == "REQUEST_NOT_PROCESSED":
+        return "error occurred: " + API_Data["status"]
+
+    # Parsing API_Data for unemployment rate and storing it
+    value = API_Data["Results"]["series"][0]["data"][0]["value"]
+    message = "The " + data_name + " for FIPS code " + str(state_fips) + str(county_fips) + " is " + str(value)
+
+    # Json that will be returned
+    response_map = {
+        "status": "success",
+        data_name : value,
+        "message": message
+    }
+
+    response_json = json.dumps(response_map)
+
+    return response_json
+    # return message
+
+# --------------------- Endpoints -----------------------------------
+
+# Unemployment Endpoint 
 @app.route('/unemployment_rate')
+def unemployment_rate_endpoint():
+    return generic_endpoint("unemployment rate", "03")
+
+# Labor Force Endpoint 
+@app.route('/labor_force')
+def lab_for_endpoint():
+    return generic_endpoint("labor force", "06")
+
+# Unemployment Endpoint 
+@app.route('/unemployment')
 def unemployment_endpoint():
+    return generic_endpoint("unemployment", "04")
+
+# Employment Endpoint 
+@app.route('/employment')
+def employment_endpoint():
+    return generic_endpoint("enemployment", "05")
+
+
+# ------------------ Old Code (kept if needed to revert back to) ------------------
+
+# Old non-generic style if needed
+'''
+def unemployment_rate_endpoint():
     state_fips = request.args.get('state_fips')
     county_fips = request.args.get('county_fips')
     # EXAMPLE: http://127.0.0.1:5000/unemployment_rate?state_fips=01&county_fips=001
     api_url = "https://api.bls.gov/publicAPI/v2/timeseries/data/LAUCN" + str(state_fips) + str(county_fips) + "0000000003"
     print(api_url)
+
     # Making a GET request
     response = requests.get(api_url)    
     API_Data = response.json()
-    return "The unemployment rate for FIPS code " + str(state_fips) + str(county_fips) + " is " + str(API_Data["Results"]["series"][0]["data"][0]["value"]) + "%"
+
+    # Parsing API_Data for unemployment rate and storing it
+    unemployment_rate = API_Data["Results"]["series"][0]["data"][0]["value"]
+    message = "The unemployment rate for FIPS code " + str(state_fips) + str(county_fips) + " is " + str(unemployment_rate) + "%"
+
+    # Json that will be returned
+    response_map = {
+        "status": "success",
+        "unemployment_rate": unemployment_rate,
+        "message": message
+    }
+
+    response_json = json.dumps(response_map)
+
+    return response_json
+    # return message
+'''
+
+
+
+
