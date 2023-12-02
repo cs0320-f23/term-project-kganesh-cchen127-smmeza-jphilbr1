92c026293a7633237b674e024d7d0a42b0ef7917
Jay Philbrick <jay_philbrick@brown.edu>
2023-12-01 19:22:05 -0500

    feat: fixed FullData, outputted file

--- back/src/FullData.py
+++ back/src/FullData.py
@@ -9,33 +9,41 @@ def update_geojson():
 
     variables = ["unemployment_rate", "labor_force", "unemployed", "employed"]
 
-    n_iter_needed = math.ceil(len(data.features)/50)
+    n_iter_needed = math.ceil(len(data["features"])/50)
+    n_iter_needed = 1 # DELETE THIS
 
     for var_name in variables:
         for i in range(n_iter_needed):
             county_list = []
             if i != n_iter_needed - 1:
                 for j in range(50):
-                    county_list.append(str(data.features[50*i+j].properties.STATEFP)+str(data.features[50*i+j].properties.COUNTYFP))
+                    county_list.append(str(data["features"][50*i+j]["properties"]["STATEFP"])+str(data["features"][50*i+j]["properties"]["COUNTYFP"]))
             else:
-                for j in range((len(data.features)/50-math.floor(len(data.features)/50))*50):
-                    county_list.append(str(data.features[50*i+j].properties.STATEFP)+str(data.features[50*i+j].properties.COUNTYFP))
+                for j in range(int(math.floor((len(data["features"])/50-math.floor(len(data["features"])/50))*50))):
+                    county_list.append(str(data["features"][50*i+j]["properties"]["STATEFP"])+str(data["features"][50*i+j]["properties"]["COUNTYFP"]))
             
             # NOW SEND THOSE county_list to function, save that as county_map
             # retVal = [["02033","67.1"]]
 
-            if len(county_list > 50):
+            print("County List:")
+            print(county_list)
+
+            if len(county_list) > 50:
                 raise ValueError("ERROR ON 28")
             retVal = fips_to_data(var_name, county_list)
-            if len(retVal > 50):
+            if len(retVal) > 50:
                 raise ValueError("ERROR ON 31")
             
+            print("retVal:")
+            print(retVal)
+            
             for j in range(len(retVal)):
-                data.features[50*i+j].properties.update({var_name:retVal[j][1]})
+                data["features"][50*i+j]["properties"].update({var_name:retVal[j][1]})
 
+    with open("../data/all_county_geojson_with_data.json", "w") as fp:
+        json.dump(data, fp)
 
-    with open("../data/all_county_geojson_with_data.json", "w") as outfile:
-        outfile.write(data)
+    return data
 
 # Returns contents of GEOJson file with all geo-tagged data
 def full_data_retrieval():
@@ -53,3 +61,6 @@ def full_data_retrieval():
     response_json = json.dumps(response_map)
 
     return response_json
+
+
+update_geojson()
