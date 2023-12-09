bb044c46496fef27cdd64be54180f5fc3b005bc6
Jay Philbrick <jay_philbrick@brown.edu>
2023-12-09 13:41:53 -0500

    feat: removed commodityRec dependence on total employment

--- back/src/CommodityRecs.py
+++ back/src/CommodityRecs.py
@@ -1,7 +1,8 @@
 from CONSTANTS import *
 
-def recommendCommodities(employed: int, employees_mining: int, employees_construction: int, employees_manufacturing: int, employees_tradetransportutilities: int, employees_information: int, employees_finance: int, employees_professionalservices: int, employees_educationandhealth: int, employees_leisureandhospitality: int, employees_otherservices: int):
+def recommendCommodities(employees_mining: int, employees_construction: int, employees_manufacturing: int, employees_tradetransportutilities: int, employees_information: int, employees_finance: int, employees_professionalservices: int, employees_educationandhealth: int, employees_leisureandhospitality: int, employees_otherservices: int):
     employee_count_list = [employees_mining, employees_construction, employees_manufacturing, employees_tradetransportutilities, employees_information, employees_finance, employees_professionalservices, employees_educationandhealth, employees_leisureandhospitality, employees_otherservices]
+    employed = sum(employee_count_list)
     heavy_industries_list = []
 
     for i in range(len(employee_count_list)):
