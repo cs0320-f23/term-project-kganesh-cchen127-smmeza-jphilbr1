1d6e61df6ef11f30649be2919e0c4c055a2ebfa2
Kylash Ganesh <kylash_ganesh@brown.edugit config --global user.name kylashg1>
2023-12-15 17:19:19 -0500

    feat: began testing mocks

---
+++ back/test/test_mocks.py
@@ -0,0 +1,32 @@
+from BLS_mocks import *
+
+import unittest
+import sys
+
+sys.path.insert(0, '../src')
+
+from ApiBLS import * 
+
+
+class TestMocks(unittest.TestCase):
+
+    # tests that API receives a response for Autauga County, AL with:
+    # - successful response
+    # - non-negative statistic
+    # - matching state and county fips as the ones that were queried
+    # - non-empty response message
+
+    def test_Mocks(self):
+        res = json.loads(mock_function("unemployment_rate_mock"))
+        self.assertEqual(res["status"], "success")
+        self.assertTrue(float(res["unemployment_rate"]) >= 0)
+        self.assertEqual(res["state_fips"], "01")
+        self.assertEqual(res["county_fips"], "001")
+        self.assertTrue(len(res["message"]) > 0)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
