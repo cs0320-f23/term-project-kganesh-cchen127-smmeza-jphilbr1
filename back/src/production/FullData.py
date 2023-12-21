3e025be2ce28db93600ead5d5275dcd50235da5c
Jay Philbrick <jay_philbrick@brown.edu>
2023-12-21 12:27:07 -0500

    docs: added commenting to FullData.py

--- back/src/production/FullData.py
+++ back/src/production/FullData.py
@@ -2,20 +2,26 @@ import json
 import math
 from .ApiBLS import * 
 
+
+# Updates the all_county_geojson_with_data.json with updated BLS data via calls to BLS API
 def update_geojson():
     # Loading in all_county_geojson data
     f = open('./data/all_county_geojson.json')
     data = json.load(f)
     f.close
-
+    # List of all relevant data points
     variables = ["unemployment_rate", "labor_force", "unemployed", "employed"]
 
+    # BLS API limits calls to 50 series at a time -- need to run this many API calls for each variable
     n_iter_needed = math.ceil(len(data["features"])/50)
+
 
     for var_name in variables:
         for i in range(n_iter_needed):
             print("VARIABLE: " + str(var_name) + " " + "BATCH NUMBER: " + str(i))
             county_list = []
+
+            # if not at end of set of calls for variable list, add 50 calls to next batch. Otherwise, add any remaining.
             if i != n_iter_needed - 1:
                 for j in range(50):
                     county_list.append(str(data["features"][50*i+j]["properties"]["STATEFP"])+str(data["features"][50*i+j]["properties"]["COUNTYFP"]))
@@ -27,17 +33,19 @@ def update_geojson():
             print(county_list)
 
             if len(county_list) > 50:
-                raise ValueError("ERROR ON 28")
+                raise ValueError("ERROR ON 36")
             retVal = fips_to_data(var_name, county_list)
             if len(retVal) > 50:
-                raise ValueError("ERROR ON 31")
+                raise ValueError("ERROR ON 39")
             
             print("retVal:")
             print(retVal)
             
+            # update county GeoJSON with this newly-acquired economic variable
             for j in range(len(retVal)):
                 data["features"][50*i+j]["properties"].update({var_name:retVal[j][1]})
 
+    # Save new full file in directory
     with open("./data/all_county_geojson_with_data.json", "w") as fp:
         json.dump(data, fp)
 
